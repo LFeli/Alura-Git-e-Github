d87cd9c1410ec56d548a72448bc04e1d5a069013
Lucas Felipe <lucasfelipemk@gmail.com>
2022-08-29 16:23:44 -0300

    adicionando um console log

--- sistema-de-cadastro/app.js
+++ sistema-de-cadastro/app.js
@@ -2,3 +2,4 @@ console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("A aplicação está se conectando ao banco de dados")
 // Mais um console.log de teste
 console.log("O lucas inseriu essa linha no app.js")
+console.log("Enviando para o github")
